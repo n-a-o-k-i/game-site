ffc4685451a3aa2c2782da859112b379922d71f3
Naoki Terauchi <nao.rami.1124@i.softbank.jp>
2018-10-02 16:57:23 +0900

    ユーザーまわりの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'posts/index'
+  get 'users/index' => "users#index"
+  get "signup" => "users#new"
+  get "users/:id" => "users#show"
+  post "users/create"  => "users#create"
+  get "users/:id/edit" => "users#edit"
+  post "users/:id/update" => "users#update"
+
+  get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
   get "posts/:id" => "posts#show"
   post "posts/create"  => "posts#create"
